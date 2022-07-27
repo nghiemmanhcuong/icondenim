b73abda74740e6331e527d3e34d45f4458ab5970
nghiemmanhcuong <89817061+nghiemmanhcuong@users.noreply.github.com>
2022-07-27 22:58:53 +0700

    set up in client

---
+++ client/views/block/topbar.php
@@ -0,0 +1,27 @@
+<div class="topbar">
+    <div class="topbar-block">
+        <p class="topbar-block-text active">Thanh toán khi nhận hàng</p>
+        <p class="topbar-block-text">Freeship toàn quốc cho đơn từ 500K</p>
+        <p class="topbar-block-text">Hàng mới mỗi ngày</p>
+    </div>
+    <div class="topbar-block">
+        <div class="topbar-block-item">
+            <div class="topbar-block-icon">
+                <div class="ring">
+                    <div class="coccoc-alo-phone coccoc-alo-green coccoc-alo-show">
+                        <div class="coccoc-alo-ph-circle"></div>
+                        <div class="coccoc-alo-ph-circle-fill"></div>
+                        <div class="coccoc-alo-ph-img-circle">
+                            <i class="fa-solid fa-phone"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="topbar-block-hotline">
+                <a href="tel:0987954221">
+                    Hotline: 0987.954.221
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
