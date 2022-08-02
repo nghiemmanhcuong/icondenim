59512c5e7c33a373b10a693dc03e2a16259d9dd4
quangvc <109118553+quangvc@users.noreply.github.com>
2022-08-02 11:34:09 +0700

    Update index.php

--- admin/views/shop_info/index.php
+++ admin/views/shop_info/index.php
@@ -1,31 +1,31 @@
 <div class="content">
         <form action="">
-            <div class="form-group">
-                <label for="Hotline">Hotline</label>
+            <div class="form-group mb-3">
+                <label for="Hotline"> <strong>Hotline</strong> </label>
                 <input type="text" value="<?= $shop_info['hotline'] ?>" class="form-control" id="Hotline">
             </div>
-            <div class="form-group">
-                <label for="Logo">Logo</label>
+            <div class="form-group mb-3">
+                <label for="Logo"><strong>Logo</strong></label>
                 <input type="text" value="<?= $shop_info['logo_image'] ?>" class="form-control" id="Logo">
             </div>
-            <div class="form-group">
-                <label for="email-lh">Email liên hệ</label>
+            <div class="form-group mb-3">
+                <label for="email-lh"><strong>Email liên hệ</strong></label>
                 <input type="text" value="<?= $shop_info['email_contact'] ?>" class="form-control" id="email-lh">
             </div>
-            <div class="form-group">
-                <label for="lhbh">Liên hệ bán hàng</label>
+            <div class="form-group mb-3">
+                <label for="lhbh"><strong>Liên hệ bán hàng</strong></label>
                 <input type="text" value="<?= $shop_info['co-operate_phone'] ?>" class="form-control" id="lhbh">
             </div>
-            <div class="form-group">
-                <label for="truso">Tư vấn bán hàng</label>
+            <div class="form-group mb-3">
+                <label for="truso"><strong>Tư vấn bán hàng</strong></label>
                 <input type="text" value="<?= $shop_info['sales_consultant_phone'] ?>" class="form-control" id="truso">
             </div>
-            <div class="form-group">
-                <label for="email">Trụ sở chính</label>
+            <div class="form-group mb-3">
+                <label for="email"><strong>Trụ sở chính</strong></label>
                 <input type="text" value="<?= $shop_info['headquarters_address'] ?>" class="form-control" id="email">
             </div>
-            <div class="form-group">
-                <label for="email">email</label>
+            <div class="form-group mb-3">
+                <label for="email"><strong>Email</strong></label>
                 <input type="text" value="<?= $shop_info['email'] ?>" class="form-control" id="email">
             </div>
         </form>
